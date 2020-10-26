84d846547718ba6c67cc05e8d78e95cfa8534738
Ta-Mo <62438316+Ta-Mo@users.noreply.github.com>
2020-10-26 20:51:05 +0100

    add ReactMarkdown component to author.js and index.js

--- src/templates/article.js
+++ src/templates/article.js
@@ -2,7 +2,7 @@ import React from "react"
 import { Link, graphql } from "gatsby"
 import Img from "gatsby-image"
 import Layout from "../components/layout"
-import ReactMarkdown, { uriTransformer } from "react-markdown"
+import ReactMarkdown from "react-markdown"
 
 const ArticleTemplate = ({ data }) => (
   <Layout>
